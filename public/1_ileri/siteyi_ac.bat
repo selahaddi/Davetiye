ae0a2c54e1821bd46c7396153422c86ac027ec0f
selahaddi <selahaddi@users.noreply.github.com>
2026-07-03 21:47:50 +0300

    fix: restore baslangic, orta and ileri templates with their supabase configurations

--- public/1_ileri/siteyi_ac.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Davetiye Baslatici
-echo Siteniz tarayicida aciliyor...
-cd /d "%~dp0"
-start "" "davetiye_giri\code.html"
-exit
--- public/2_orta/siteyi_ac.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" "%~dp0code.html"
-exit
